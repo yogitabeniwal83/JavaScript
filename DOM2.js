5a8d5337142945198f10d6cf51b2e39c8a4e1376
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-09-27 23:41:34 +0530

    Add new DOM js file

---
+++ DOM2.js
@@ -0,0 +1,26 @@
+// let div = document.querySelector("div");
+// console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// let name = div.getAttribute("name");
+// console.log(name);
+
+
+// let para = document.querySelector("p");
+// console.log(para.getAttribute("class"));
+
+// let para = document.querySelector("p");
+// console.log(para.setAttribute("class","newClass"));
+
+// let name = document.querySelector("name");
+// console.log(div.setAttribute("JSDiv","newJSDiv"));
+
+let div = document.querySelector("div");
+div.style.backgroundColor = "green";
+// div.style.visibility = "hidden";
+
+div.style.fontSize = "26px";
+
+div.innerText = "hello";
