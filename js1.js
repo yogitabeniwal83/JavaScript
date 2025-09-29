8f2de5363a51aebc31680bb3391d9b4362bd7622
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-09-29 22:17:57 +0530

    Add new js file

---
+++ js1.js
@@ -0,0 +1,58 @@
+console.log("hello js");
+console.log("yogita");
+fullname="tony";
+console.log(fullname);
+age=24;
+console.log(age);
+price=150.50;
+console.log(price);
+x=null;
+console.log(x);
+y=undefined;
+console.log(y);
+isFollow=true;
+console.log(isfollow);
+isFollow=false;
+console.log(isfollow);
+fullname="yogita";
+console.log(fullname);
+fullName="yogesh";
+console.log(fullName);
+fullname=rinku;
+_fullname=ravi;
+$fullname=sona;
+let full_Name="tony";
+console.log(fullName);
+let age1=24;
+console.log(age);
+totalPrice=1000;
+console.log(totalPrice);
+var age=24;
+var age=40;
+var age=57;
+console.log(age);
+let age=20;
+age=30;
+age=45;
+console.log(age);
+const PI=3.14;
+console.log(PI);
+const c=10;
+console.log(a);
+let age=24;
+let price=100.50;
+let fullName="tony";
+isFollow=true;
+let x;
+let y=null;
+let z = BigInt("123");
+let a=Symbol("hello!");
+const student={
+    fullName : "yogita beniwal",
+    age : 18,
+    cgpa : 9.3,
+    isPass : true,
+};
+student["age"]=student["age"]+1;
+student["fullName"]="yogita";
+console.log(student.fullName);
