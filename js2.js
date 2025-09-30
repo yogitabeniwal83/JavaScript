e710f415bc3118f5eae1a8b994bd2326f3d94a2f
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-09-30 22:29:12 +0530

    Add new js file

---
+++ js2.js
@@ -0,0 +1,11 @@
+// Arithematic operation
+let a=5;
+let b=2;
+
+console.log("a=",a ,"$ b=",b);
+console.log("a + b = ",a + b);
+console.log("a - b=",a - b);
+console.log("a * b=",a * b);
+console.log("a / b=",a / b);
+console.log("a % b=",a % b);
+console.log("a ** b=",a ** b);    //5^2=25
