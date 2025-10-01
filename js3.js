1666f132539633fcb2bb853c7fa78ae9aa4367bd
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-10-01 20:07:58 +0530

    Add new js file

---
+++ js3.js
@@ -0,0 +1,19 @@
+// unary operator
+
+let a=5;
+let b=2;
+
+console.log("a=",a ,"$ b=",b);
+a++;//6
+// a--;
+console.log("a = ",a);//6
+a=5;
+a--;//4
+console.log("a = ",a);//4
+a=5;
+console.log("a++ = ",a++);//5
+console.log("a=",a);//6
+console.log("++a = ",++a);//7
+console.log("--a = ",--a);
+console.log("a-- = ",a--);
+console.log("a = ",a);
