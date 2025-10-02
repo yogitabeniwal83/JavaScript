a429834b85502289edefdbf48306cc6782b71c42
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-10-02 22:25:52 +0530

    Add new js file

---
+++ js4.js
@@ -0,0 +1,22 @@
+// assignment operator
+
+let a=5;
+let b=2;
+
+a += 4;//a=a+4
+console.log("a = ",a);//9
+a=5;
+a -= 2;//a=a-2
+console.log("a = ",a);//3
+a=5;
+a *= 5;//a=a*5
+console.log("a = ",a);//25
+a=5;
+a /= 2;//a=a/2
+console.log("a = ",a);//2.5
+a=5;
+a**=4;//a=a**4
+console.log("a = ",a);
+a=5;
+a %= 2;//a=a%2
+console.log("a = ",a);
