b99339852668c8cd0184ff10ce3fe74311ac4abb
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-10-03 23:32:20 +0530

    Add new js file

---
+++ js5.js
@@ -0,0 +1,27 @@
+// comparison operator
+
+let a=5;
+let b=2;
+
+console.log("5 == 2",a == b);//false
+console.log("5 != 2",a != b);//true
+
+let c = 5;//number
+let d = "5";//string -> number
+
+console.log("5 == 5", c == d);//true
+
+let x = 5;//number
+let y = "5";//string
+
+console.log("5 === 5", x === y);//false
+console.log("5 !== 5", x !== y);//false
+
+let p = 5;
+let q = 3;
+
+console.log("5 > 3", a > b);
+console.log("5 >= 3", a >= b);
+console.log("5 < 3", a < b);
+console.log("5 <= 3", a <= b);
+
